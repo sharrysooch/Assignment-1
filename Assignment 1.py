0ed82eb553d6b7b0f6bd1811271a715820d54af0
sharrysooch <74150464+sharrysooch@users.noreply.github.com>
2020-11-08 23:20:28 +0530

    Assignment 1

---
+++ Assignment 1.py
@@ -0,0 +1,16 @@
+#1 RAINBOW (B)
+#2 Print "LETS UPGRADE"
+print("LETS UPGRADE")
+#3 Profit/Loss or Neither
+cp=int(input())
+sp=int(input())
+if cp<sp:
+    print("Profit")
+elif cp>sp:
+    print("Loss")
+else:
+    print("Neither")
+#4 Euros to Rupees
+euro=int(input())
+rupees=euro*80
+print(rupees)
